207ffab0950a6f7e229da9149df92fc2925f0a2d
Erik Ihrén <erikihr@gmail.com>
2015-08-28 21:59:01 +0200

    Added comments and faulty 5.3

--- CTCI/u5.py
+++ CTCI/u5.py
@@ -1,7 +1,9 @@
+# 5.1 Insert M into N between bit i and j.
 def insert(N,M,i,j):
     return (M << i) | N
     
 
+# 5.2 Print a real number between 0 and 1 in binary representation
 def decbin(r):
     result = "0."
     start = 0.5
@@ -18,3 +20,33 @@ def decbin(r):
         return "0"
     return result
 
+# 5.3 Print the next smallest and largest number with same amount of 1 bits
+# Misinterpreted question and forgot part about keeping same amount of bits.
+def print_binary(b):
+    print int("".join(b),2)
+
+def onebits(i):
+    binary = list(bin(i)[2:])# Convert to binary and remove prefix
+    print binary
+    first_zero = -1
+    for i,bit in enumerate(binary):
+        if bit == "0":
+            first_zero = i
+
+    if first_zero == -1:
+        print_binary(list("1"+ "0" * len(binary)))
+        binary[len(binary)-1] = "0"
+        print_binary(binary)
+        return
+
+    binary[first_zero] = "1" 
+    binary2 = binary[:]
+    if first_zero > 0:
+        binary[first_zero-1] = "0"
+    else:
+        binary = ["1"] + binary
+    if first_zero < len(binary2) - 1:
+        binary2[first_zero+1] = "0"
+
+    print_binary(binary2)
+    print_binary(binary)
